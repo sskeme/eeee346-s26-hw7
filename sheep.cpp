8bdb74c7650880a1049745a783b74d05b88de578
Sunny Kim <154963758+sskeme@users.noreply.github.com>
2026-03-15 19:24:39 -0400

    Create sheep.cpp

---
+++ sheep.cpp
@@ -0,0 +1,11 @@
+#include "sheep.h"
+
+// TODO: write your code to initialize the static member variable
+
+// TODO: write your code of the constructor
+
+// TODO: write your code of the copy constructor
+
+// TODO: write your code of the member functions (incrementAge() and setWeight(int weight))
+
+// TODO: write your code of the overloaded << operator
