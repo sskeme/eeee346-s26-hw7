fab4f684f81de069100a2dedbd5d70c5a2132917
Sunny Kim <154963758+sskeme@users.noreply.github.com>
2026-03-15 19:23:31 -0400

    Create cow.cpp

---
+++ cow.cpp
@@ -0,0 +1,9 @@
+#include "cow.h"
+
+// TODO: write your code to initialize the static member variable
+
+// TODO: write your code of the constructor
+
+// TODO: write your code of the member functions (incrementAge() and setWeight(int weight))
+
+// TODO: write your code of the overloaded << operator
